c5f382d6e61ad2640f4d520e1cfb553739035f9a
Matthew Ulman <matthew.ulman@rutgers.edu>
2020-05-05 23:23:45 -0400

    First commit

---
+++ src/build/precache-manifest.feac81d38a20a9bafb47c39779a5e0d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278c6e8e12206b17424d3644366d7f41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47043e07a87d25b8b0eb",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "0cf6d6148858952f572d",
+    "url": "/static/js/2.f2b8de2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2b8de2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47043e07a87d25b8b0eb",
+    "url": "/static/js/main.a7af64c8.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
